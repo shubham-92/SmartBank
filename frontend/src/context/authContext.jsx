e8349b21430bc85e371a9d922adf5531718a6197
shubham-92 <shubhamsj2933@gmail.com>
2026-02-01 12:07:55 +0530

    Fix authContext filename

--- frontend/src/context/authContext.jsx
+++ frontend/src/context/authContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useState } from "react";
 
-export const authContext = createContext(null);
+export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem("token"));
