f71bc94cd51072dd83f5b26343f0e14421e87c01
shubham-92 <shubhamsj2933@gmail.com>
2026-02-01 12:44:20 +0530

    Update CORS middleware to allow all origins and disable credentials

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,9 +13,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[ "http://localhost:5173",
-        "https://smart-bank-inky.vercel.app",],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
