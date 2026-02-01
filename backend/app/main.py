7b8a7644f7e3c97845ca0588216ca8b3bb16c9f6
shubham-92 <shubhamsj2933@gmail.com>
2026-02-01 12:25:07 +0530

    Fix CORS allow_origins URL for production deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[ "http://localhost:5173",
-        "https://smartbank.vercel.app",],
+        "https://smart-bank-inky.vercel.app",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
